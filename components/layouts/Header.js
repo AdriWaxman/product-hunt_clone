33d491ef2c0c0e7ef51aab3ad968c6a8a8f2161d
AdriMan <adrimpartner@gmail.com>
2022-02-20 20:19:49 +0100

    Created about page and new components

---
+++ components/layouts/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Search from "../ui/Search";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <p>P</p>
+          <Search />
+
+        </div>
+        
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Layout from '../components/layouts/layout';
+import styled from '@emotion/styled';
+
+
+const About = () => {
+  return ( 
+    <div>
+      <Layout>
+        <h1>About us</h1>
+        <p>This is the about page</p>
+      </Layout>
+    </div>
+   );
+}
+ 
+export default About;
